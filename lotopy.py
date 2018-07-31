684b0ddd68df0829a9f64ca9fc3949c9d470b576
Artur C. Grover <artur.turbas@gmail.com>
2018-07-31 08:42:03 -0300

    Moved commands to functions

--- lotopy.py
+++ lotopy.py
@@ -3,29 +3,45 @@ import zipfile
 import io
 import pandas as pd
 import numpy as np
-from bs4 import BeautifulSoup
 
+# URL to zipfile where lotofacil runs can be found
 url = 'http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_lotfac.zip'
-r = requests.get(url)
-z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall()
 
-df_loto = pd.read_html('D_LOTFAC.HTM')[0]
-df_loto = pd.DataFrame(df_loto)
-header = df_loto.iloc[0]
-df_loto = df_loto[1:]
-df_loto.rename(columns = header, inplace = True)
-df_loto.drop(['Cidade', 'UF'], axis = 1, inplace = True)
-df_loto.dropna(inplace = True)
-df_loto.set_index('Concurso', inplace = True)
-
+# My lucky numbers :)
 jogo1 = np.array(['01', '03', '04', '06', '09', '10', '11', '12', '14', '17', '18', '20', '21', '23', '25'])
 jogo2 = np.array(['02', '04', '05', '05', '10', '11', '13', '14', '15', '16', '19', '21', '22', '23', '24'])
 
+# Columns name with withdrawn numbers
 bolas = ['Bola1', 'Bola2', 'Bola3', 'Bola4', 'Bola5', 'Bola6', 'Bola7', 'Bola8', 'Bola9', 'Bola10', 'Bola11', 'Bola12', 'Bola13', 'Bola14', 'Bola15']
 
-sorteio = df_loto[bolas]
 
+def extract_page(url):
+    r = requests.get(url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall()
+
+
+def create_df():
+    df = pd.read_html('D_LOTFAC.HTM')[0]
+    df = pd.DataFrame(df)
+    header = df.iloc[0]
+    df = df[1:]
+    df.rename(columns = header, inplace = True)
+    df.drop(['Cidade', 'UF'], axis = 1, inplace = True)
+    df.dropna(inplace = True)
+    df.set_index('Concurso', inplace = True)
+
+    return df
+
+def select_cols(df, cols = bolas):
+    return df[cols]
+
+extract_page(url)
+df = create_df()
+sorteio = select_cols(df, bolas)
+
+concurso = sorteio.index[-1]
+print(concurso)
 numbers = np.array(sorteio.iloc[-1].values)
 count1 = 0
 count2 = 0
